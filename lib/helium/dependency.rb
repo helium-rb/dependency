5c89478d83c3c6f54d84ebf378964766868af37e
Stanislaw Klajn <sklajn@gmail.com>
2021-07-09 00:07:27 +0100

    Empty gem

---
+++ lib/helium/dependency.rb
@@ -0,0 +1,8 @@
+require "helium/dependency/version"
+
+module Helium
+  module Dependency
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/helium/dependency/version.rb
@@ -0,0 +1,5 @@
+module Helium
+  module Dependency
+    VERSION = "0.1.0"
+  end
+end
